ea168d3adb6deee832811ccaee0dee18f403237f
JORAWARSINGHSAINI <jorawarsingh12@gmail.com>
2020-05-25 19:37:54 +0530

    Updates

---
+++ precache-manifest.ff4b45a718f100ed205c7c2a88dbaf01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0169177524a15df4f0532398e571fe91",
+    "url": "/CoronaCounter/index.html"
+  },
+  {
+    "revision": "6749a4f289f5c61f56a1",
+    "url": "/CoronaCounter/static/css/main.28f12668.chunk.css"
+  },
+  {
+    "revision": "6434e99fd906b627cec1",
+    "url": "/CoronaCounter/static/js/2.e7bf167f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/CoronaCounter/static/js/2.e7bf167f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6749a4f289f5c61f56a1",
+    "url": "/CoronaCounter/static/js/main.9ecdd243.chunk.js"
+  },
+  {
+    "revision": "06cff63d32854287f5be",
+    "url": "/CoronaCounter/static/js/runtime-main.91407152.js"
+  }
+]);
